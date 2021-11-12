8955c8ece231af14e54c1672daf3481d41356e01
formidablae <81068781+formidablae@users.noreply.github.com>
2021-11-12 22:44:15 +0100

    Added routes for dashboard root and manage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ManageComponent } from './components/manage/manage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+      path: '',
+      component: DashboardComponent,
+  },
+  {
+      path: 'manage',
+      component: ManageComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
